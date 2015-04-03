3d383a63d5d6415f7f1d2e557890188c2c1ede78
柯林 <colin1124x@gmail.com>
2015-04-03 15:57:19 +0800

    .

--- fun.sh
+++ fun.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+c=789
+export d=0
+
 fun() {
-  for n in $@; do
-    echo $n;
-  done
+	b=456
+ 	local a=123
+ 	./val.sh $a
+ 	echo in fun: a = $a, b = $b, c = $c, d = $d
+ 	echo
 }
 
-fun 9 5 1
-
+fun
---
+++ val.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo in val: a = $a
+echo in val: b = $b
+echo in val: c = $c
+echo in val: d = $d
+echo in val: $1
